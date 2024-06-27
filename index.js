f2feb8f3f87bf7748af874681f051e4dd6dc87eb
WinnieAtieno <winnieotieno773@gmail.com>
2024-06-27 22:18:57 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideFeet){
   // Write your code here!
+  if(rideFeet <= 400){
+    return 'This one is on me!'
+  }
+  else if(rideFeet > 400 && rideFeet <= 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if(rideFeet > 2000 && rideFeet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else  if (rideFeet > 2500){
+    return 'No can do.'
+  }
+}
+console.log(scuberGreetingForFeet(2600))
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === 'NYC')? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous': return "Thank you so much.";
+    break;
+    case 'not as generous': return "Thank you.";
+    break;
+    default: return "Bye.";
+  }
 }
